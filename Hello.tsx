b27bfa9447392a87378d3eaf8e7646340b2be9df
ka-tanaka-razoredge <software@razor-edge.net>
2020-09-30 13:01:56 +0900

    Initial commit

---
+++ Hello.tsx
@@ -0,0 +1,3 @@
+import React from 'react';
+
+export default ({ name }) => <div style={{border: '1px solid black'}}><h1>Hello {name}!</h1></div>;
---
+++ README.md
@@ -0,0 +1,3 @@
+# react-ts-expander
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/react-ts-expander)
